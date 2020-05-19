95d8c0d9a314b46a930dec38eb75656002150cf1
Mukesh Shakya <mukeshakya11@gmail.com>
2020-05-19 16:02:25 +0545

    Prevention list shown

--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -9,87 +9,168 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      child: ListView(
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'Covid-19',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Are you feeling sick?',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'If you feel sick with any of covid-19 symptoms please call or sms us immediately',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    height: 50,
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Container(
-                            alignment: Alignment.center,
-                            height: 50,
-                            child: Text(
-                              'Call now',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 16),
-                            ),
-                            decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(25)),
-                          ),
-                        ),
-                        SizedBox(width: 20),
-                        Expanded(
-                          child: Container(
-                            alignment: Alignment.center,
-                            height: 50,
-                            child: Text(
-                              'Send SMS',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 16),
-                            ),
-                            decoration: BoxDecoration(
-                                color: Colors.blueAccent,
-                                borderRadius: BorderRadius.circular(25)),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 30)
-                ],
-              ),
-              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-            ),
+            _getHeaderView(),
+            SizedBox(height: 30),
+            _getPreventionView(),
             Expanded(
-              child: Container(color: Colors.blue),
+              child: Container(),
             )
           ]),
+    );
+  }
+
+  Widget _getHeaderView() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Covid-19',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Are you feeling sick?',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            'If you feel sick with any of covid-19 symptoms please call or sms us immediately',
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 50,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.call,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Call now',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.message,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Send SMS',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(25)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 30)
+        ],
+      ),
+      decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(40),
+              bottomRight: Radius.circular(40))),
+    );
+  }
+
+  Widget _getPreventionView() {
+    double _cellHeight = (MediaQuery.of(context).size.width - 30) / 3;
+    var preventionList = [
+      {'image': 'images/wash_hands.png', 'msg': 'Avoid close contact'},
+      {'image': 'images/wash_hands.png', 'msg': 'Clean your hands often'},
+      {'image': 'images/wash_hands.png', 'msg': 'Wear a face mask'},
+    ].toList();
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            'Prevention',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          height: _cellHeight,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  width: _cellHeight,
+                  child: Column(children: <Widget>[
+                    Image.asset(
+                      preventionList[index]['image'],
+                      width: _cellHeight - 42,
+                      height: _cellHeight - 42,
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      preventionList[index]['msg'],
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold
+                      ),
+                    )
+                  ]),
+                );
+              },
+              itemCount: preventionList.length,
+          )
+        )
+      ]
     );
   }
 }
